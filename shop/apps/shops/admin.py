6ce6cf5ba24f9ea84c2f4a6f2dc5da3216eb58f4
antonida <anotnida619@gmail.com>
2020-08-11 17:27:43 +0400

    City and Shop

--- shop/apps/shops/admin.py
+++ shop/apps/shops/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'street', 'house', 'time_open', 'time_close', 'created_at', 'update_at')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', )
--- shop/apps/shops/models.py
+++ shop/apps/shops/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    # city = ManyToManyField(City, verbose_name='Город')
+    street = models.CharField(max_length=255, verbose_name='Улица')
+    house = models.CharField(max_length=255, verbose_name='Дом')
+    time_open = models.TimeField(verbose_name='Время открытия')
+    time_close = models.TimeField(verbose_name='Время закрытия')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Обновленно')
+
+    class Meta:
+        verbose_name = 'Магазин'
+        verbose_name_plural = 'Магазины'
+        ordering = ['created_at']
+
+    def __str__(self) -> str:
+        return self.title
