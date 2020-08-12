87483d68dca98b5d54ddca62f858e190a399ee32
antonida <anotnida619@gmail.com>
2020-08-12 22:12:00 +0400

    Add validation save_model City for Street

--- shop/apps/shops/models.py
+++ shop/apps/shops/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from apps.city.models import City
+from apps.street.models import Street
+from django.http import HttpResponse
 
 
 class Shop(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
-    city = models.ForeignKey(City, on_delete=models.CASCADE, null=False, verbose_name='Город')
-    street = models.ForeignKey(City, on_delete=models.CASCADE, null=False, verbose_name='Город')
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, verbose_name='Город')
+    street = models.ForeignKey(Street, on_delete=models.SET_NULL, null=True, verbose_name='Улица')
     house = models.CharField(max_length=255, verbose_name='Дом')
-    time_open = models.TimeField(verbose_name='Время открытия')
-    time_close = models.TimeField(verbose_name='Время закрытия')
+    time_open = models.TimeField(verbose_name='Время открытия', help_text='время в формате  00:00')
+    time_close = models.TimeField(verbose_name='Время закрытия', help_text='время в формате  00:00')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     update_at = models.DateTimeField(auto_now=True, verbose_name='Обновленно')
 
@@ -19,3 +21,4 @@ class Shop(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
---
+++ shop/apps/street/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Street)
+class StreetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'city',  'created_at', 'update_at')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', )
+
