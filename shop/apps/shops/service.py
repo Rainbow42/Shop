d01fb91d9c4d1976c9872fd3d61a73ed4610e72d
antonida <anotnida619@gmail.com>
2020-08-16 22:29:49 +0400

    Update service

--- shop/apps/shops/service.py
+++ shop/apps/shops/service.py
@@ -6,10 +6,9 @@ from .models import Shop
 
 
 class ShopFilter(filters.FilterSet):
-    title = CharFilter(lookup_expr='icontains')
     city = CharFilter(field_name='city__title')
     street = CharFilter(field_name='street__title')
 
     class Meta:
         model = Shop
-        fields = ['title', 'city', 'street']
+        fields = ['city', 'street']
